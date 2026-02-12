f5e4c6fa22dabc8ebad41ae94508d75f605f24dc
Juanma <juanma.soruco@gmail.com>
2026-02-12 00:32:32 -0400

    initial

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,17 @@
-import sumar from "./sumador";
+import { sumar, multiplicar } from './sumador';
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const first = document.querySelector('#primer-numero');
+const second = document.querySelector('#segundo-numero');
+const form = document.querySelector('#sumar-form');
+const div = document.querySelector('#resultado-div');
+const divMultiplicar = document.querySelector('#resultado-multiplicar-div');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = '<p>' + sumar(firstNumber, secondNumber) + '</p>';
+  divMultiplicar.innerHTML = '<p>' + multiplicar(firstNumber, secondNumber) + '</p>';
 });
