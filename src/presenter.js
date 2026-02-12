3d015b91749bb766e79bcb792ac50287d1136e65
Juanma <juanma.soruco@gmail.com>
2026-02-12 01:21:31 -0400

    homework fix

--- src/presenter.js
+++ src/presenter.js
@@ -3,20 +3,25 @@ import { sumar, multiplicar } from './sumador';
 const first = document.querySelector('#primer-numero');
 const second = document.querySelector('#segundo-numero');
 const form = document.querySelector('#sumar-form');
-const form2 = document.querySelector('#multiplicar-form');
 const div = document.querySelector('#resultado-div');
 const divMultiplicar = document.querySelector('#resultado-multiplicar-div');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
+  const botonPresionado = event.submitter.id;
+
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = '<p>' + sumar(firstNumber, secondNumber) + '</p>';
+  if(botonPresionado === 'sumar-button') { div.innerHTML = '<p>' + sumar(firstNumber, secondNumber) + '</p>'; }
+  if(botonPresionado === 'multiplicar-button') { div.innerHTML = '<p>' + multiplicar(firstNumber, secondNumber) + '</p>'; }
+
+
+  
 });
 
-form2.addEventListener('submit', (event) => {
+form.addEventListener('submit2', (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
